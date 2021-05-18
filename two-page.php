c06286460344f8a90020ae585e90da60e8976adb
Rashad <salehov_1970@mail.ru>
2021-05-18 18:35:55 +0400

    yeni duzelis

--- two-page.php
+++ two-page.php
@@ -1,7 +1,34 @@
-<<<<<<< HEAD
-Salam two
-=======
-<?php
-echo "ikinci sehife";
+<?php 
+
+$user_mail = ['elmir@mail.ru','rashad@mail.ru'];
+$user_pass = ['123456','123456'];
+
+print_r($user_pass);
+
+$mail= trim(@$_POST['user_mail']);
+$pass = trim(@$_POST['user_pass']);
+$button = @$_POST['user_button'];
+
+if($button AND @$_GET['token']='35621ru9834658923gh69873j54g672fj34dj'){
+    if( in_array($mail,$user_mail) AND in_array($pass,$user_pass)){
+        $info = array(
+            "code" => 200, 
+            "message" => "Giriş başarılı"
+        ); 
+        $info['user'][] = ["user_mail"=>$mail,"user_pass"=>$pass];
+
+        echo $info = json_encode($info);       
+
+        header('Location:');
+    }
+    else{
+        $info = array(
+            "code" => 700, 
+            "message" => "Istifadəçi adı və ya şifrə yanlışdır."
+        ); 
+        echo $info = json_encode($info);   
+          header('Location:');
+    }
+}
+
 ?>
->>>>>>> bf81d72316bb82054131df422b665cbd9c7dd983
